c8e059331b57834d22f479a0166ddc11f7b1cce3
Alyona Vyshnevska <alyona.vyshnevska@gmail.com>
2020-05-14 20:15:58 +0200

    now able to display all items in database

--- app.py
+++ app.py
@@ -5,17 +5,13 @@ from flask import Flask, render_template, jsonify, Response, make_response
 from flask_api import status
 from flask_cors import CORS
 from pymongo import MongoClient
+# from flask_pymongo import PyMongo
 import pprint
-from bson.json_util import loads, dumps
+# from bson.json_util import loads, dumps
 
 
 # create a MongoClient to the running mongod instance on the default host and port
 client = MongoClient()
-
-# test data
-d = {"02": {"title": "I'd had quite a lot to drink and broke into song"}, 
-    "01": {"title": "I put on perfume for a video call"}, 
-    "03": {"title": "He seems like he’d give good hugs"}}
 
 # connect to mongoDB
 db = client.homemade_bread
@@ -32,33 +28,30 @@ CORS(app)
 # the default page (nothing after the slash), then the function below will get activated.
 def home():
     return "Hi"
-    # return render_template("home.html")
 
 
 @app.route("/data")
 def data():
 
-    d_new = []
-    for k,v in d.items():
-        d_new.append({"id": k, "title" : v["title"]})
-
-    return jsonify(d_new), status.HTTP_200_OK
-    # return render_template("data.html") 
+    items_raw = collect.find({})
+    items = jsonify([dict(i) for i in list(items_raw)])
+    print(type(items))
+    # for i in items: 
+    #     print(i)
+    if items is not None:
+        return items, status.HTTP_200_OK
+    else:
+        return "Records not found", status.HTTP_404_NOT_FOUND
 
 
 @app.route('/data/<id>')
 def data_id(id):
 
-    # test data
-    # if id in d.keys():
-    #     d_new = {"id": id, "title" : d[id]["title"]}
-    #     return jsonify(d_new), status.HTTP_200_OK
-
-    item = collect.find_one({"id": float(id)})
+    item = collect.find_one({"_id": int(id)})
     print(item)
     if item is not None:
         # wraps json methods and provides explicit BSON conversion to JSON
-        return dumps(item), status.HTTP_200_OK
+        return item, status.HTTP_200_OK
     else:
         return "Record not found", status.HTTP_404_NOT_FOUND
 
