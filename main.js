154f72f36bfd281834434b198e691530d2d1bf44
Alyona Vyshnevska <alyona.vyshnevska@gmail.com>
2020-06-15 08:24:12 +0200

    add fadein animation

--- main.js
+++ main.js
@@ -4,20 +4,31 @@ const current = document.querySelector('#current');
 const imgs = document.querySelectorAll('.imgs img');
 const opacity = 0.6;
 
-// loop through imgs and add event listener to all of them
 
+// set opacity of first current img
+imgs[0].style.opacity = opacity;
+
+// loop through imgs and add event listener to all of them
 imgs.forEach(img => img.addEventListener('click', imgClick));
 
 // e is event
 function imgClick(e) {
-      // Reset the opacity of all imgs
-    imgs.forEach(img => (img.style.opacity = 1));
+
     // on click set the current large to the source of clicked img
-        current.src = e.target.src;
+    current.src = e.target.src;
+
+    // Add fade-in class to add imgs
+    current.classList.add('fade-in')
+
+        // Remove fade-in class after its used, so when a new img becomes 
+    // current it will do the fadein again
+    setTimeout(() => current.classList.remove('fade-in'), 500);
+
+    // Reset the opacity of all imgs
+    imgs.forEach(img => (img.style.opacity = 1));
+
     // change opacity var
-        e.target.style.opacity = opacity;
-
-
+    e.target.style.opacity = opacity;
 
 }
 
